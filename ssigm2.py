986ec2eb33593320ecfd43a4645e62e864206168
James <jdiffen1@ufl.edu>
2017-03-14 22:25:55 -0400

    update ssigm2.py

--- ssigm2.py
+++ ssigm2.py
@@ -42,41 +42,50 @@ class Parm:
 # Define class to contain weights for neural network
 class Weights:
     # Initialize parameters
-    def __init__(self, data_cols, n_hidden, uniform):
+    def __init__(self, data_cols, n_hidden):
         # Initialize number of hidden nodes
         self.n_hidden = n_hidden
-        # Initialize either uniformly distributed weights or zeros
-        if (uniform):
-            # Initialize input bias terms 
-            self.bi = np.random.uniform(-1, 1, n_hidden)
 
-            # Initialize hidden bias term 
-            self.bh = np.random.uniform(-1, 1)
+        # Initialize original input bias terms 
+        self.bi_orig = np.random.uniform(-1, 1, n_hidden)
 
-            # Initialize input weights
-            self.Wi = np.random.uniform(-.1, .1, (n_hidden, data_cols))
+        # Initialize original hidden bias term 
+        self.bh_orig = np.random.uniform(-1, 1)
 
-            # Initialize hidden weights
-            self.Wh = np.random.uniform(-.1, .1, n_hidden)
-        else:
-            # Initialize input bias terms to zero
-            self.bi = np.zeros(n_hidden)
+        # Initialize original input weights
+        self.Wi_orig = np.random.uniform(-.1, .1, (n_hidden, data_cols))
 
-            # Initialize hidden bias term 
-            self.bh = 0
+        # Initialize original hidden weights
+        self.Wh_orig = np.random.uniform(-.1, .1, n_hidden)
 
-            # Initialize input weights to zeros
-            self.Wi = np.zeros((n_hidden, data_cols))
+        # Initialize input bias terms 
+        self.bi = np.copy(self.bi_orig)
 
-            # Initialize hidden weights
-            self.Wh = np.zeros(n_hidden)
+        # Initialize hidden bias term 
+        self.bh = np.copy(self.bh_orig)
 
+        # Initialize input weights
+        self.Wi = np.copy(self.Wi_orig)
+
+        # Initialize hidden weights
+        self.Wh = np.copy(self.Wh_orig)
+
+    # Routine for setting weights and bias terms to original values
+    def reset_weights(self):
+        self.bi = np.copy(self.bi_orig)
+        self.bh = np.copy(self.bh_orig)
+        self.Wi = np.copy(self.Wi_orig)
+        self.Wh = np.copy(self.Wh_orig)
 
 class Network:
     # Initialize parameters
-    def __init__(self, eta, b_size, data_rows, data_cols, n_hidden, uniform):
-        self.w = Weights(data_cols, n_hidden, uniform)
+    def __init__(self, eta, b_size, data_rows, data_cols, n_hidden):
+        self.w = Weights(data_cols, n_hidden)
         self.p = Parm(eta, b_size, data_rows, data_cols)
+
+    # Define function for resetting weights
+    def reset_w(self):
+        self.w.reset_weights()
 
     # Define function to generate output of neural network
     def h_out(self, feature):
@@ -102,41 +111,29 @@ class Network:
             o = self.o_out(o_h)
             temp += 0.5 * (target[i] - o)**2
 
-        print(" Total error = ", temp)
+        print("  Total error = ", temp)
 
         #return temp
 
     def update_Wh (self, Err, o, o_h):
+        # Compute gradient of Error with respect to hidden bias
         temp = self.p.eta * Err * o * (1 - o)
-#        print("Wh temp = ", temp)
+        # Update hidden bias term
         self.w.bh += temp
 
-        update = o_h * temp #np.multiply(o_h, temp)
-#        print("Wh update = ", update)
-        self.w.Wh += update #np.add(self.w.Wh, update)
-#        for j in range(self.cols): #np.nditer(self.batch):
-#            self.w.Wh[j] += temp * feature[j]
+        # Compute gradient of Error with respect to hidden weights
+        update = o_h * temp 
+        # Update hidden weights
+        self.w.Wh += update
 
     def update_Wi (self, feature, Err, o, o_h):
-#        print("Err = ", Err)
-#        print("o = ", o)
-#        print("1 - o = ", 1 - o)
-#        print("o_h = ", o_h)
-#        print("1 - o_h = ", 1 - o_h)
-#        print("Err * o * (1 - o) * o_h[0] * (1 - o_h)[0] * Wh[0] = ", 
-#               Err * o * (1 - o) * o_h[0] * (1 - o_h)[0] * self.w.Wh[0])
         temp = self.p.eta * Err * o * (1 - o) * self.w.Wh * o_h * (1 - o_h)
-#        print("Wi temp = ", temp)
         self.w.bi = np.add(self.w.bi, temp)
 
         for j in range(self.w.n_hidden):
             update = feature * temp[j]
-#            print("Wi update %d = " %j)
-#            print(update)
-            self.w.Wi[j] = np.add(self.w.Wi[j], update)
-#        for j in range(self.cols): #np.nditer(self.batch):
-#            self.w.Wh[j] += temp * feature[j]
 
+            self.w.Wi[j] = np.add(self.w.Wi[j], update)
 
     # Define function for training neural network on data set
     def train(self, feature, target):
@@ -144,43 +141,30 @@ class Network:
 
         for i in range(self.p.rows):
             o_h = self.h_out(feature[i])
-#            print("o_h = ", o_h)
             o = self.o_out(o_h)
             Err = self.err(target[i], o)
-#            print(o)
-#            print(Err)        
 
-#            if (Err != 0):
-#                num_err += 1
             self.update_Wh(Err, o, o_h)
             self.update_Wi(feature[i], Err, o, o_h)
-        
-        #print("training errors = ", num_err)
 
     # Define function for testing neural network on data set
     def test(self, feature, target, test_size, data_title):
-        num_err = test_size
+        num_err = 0
 
         for i in range(test_size):
             o_h = self.h_out(feature[i])
             o = self.o_out(o_h)
             Err = self.err(target[i], o)
-#            print("o = ", o, ", t = ", target[i], ", and Err = ", Err)
-            #if (np.abs(Err) < 0.5):
-            #    num_err -= 1
             
             if(o >= 0.5):
                 o = 1
             else:
                 o = 0
-            
-            Err = self.err(target[i], o)
-#            print("o = ", o, ", t = ", target[i], ", and Err = ", Err)
 
-            if(target[i] == o):
-                num_err -= 1  
+            if(target[i] != o):
+                num_err += 1  
 
-        print(" Number of errors from", data_title, ":", num_err)
+        print("  Number of errors from", data_title, ":", num_err)
 
     # Define function for plotting test results from Nerual Network on test set
     def plot_test(self, feature, target, test_size, plot_title):
@@ -201,10 +185,10 @@ class Network:
                 x_no.append(feature[i, 0])
                 y_no.append(feature[i, 1])
 
+        # Commands for plotting classified data points
         ax = plt.gca()
-        ax.cla()        # Clear the grid for fresh plot
-
-        plt.plot(x_no[:], y_no[:], 'ro', markersize=6, lw=3)
+        ax.cla()             # Clear the grid for fresh plot
+        plt.plot(x_no[:], y_no[:], 'r^', markersize=6, lw=5)
         plt.plot(x_yes[:], y_yes[:], 'go', markersize=6, lw=3)
         circle1 = plt.Circle((0.5, 0.6), 0.4, color='k', lw=3, fill=False)
         plt.gca().set_aspect('equal', adjustable='box')
@@ -214,50 +198,64 @@ class Network:
         plt.title(plot_title)
         plt.xlabel('x')
         plt.ylabel('y')
-#        plt.legend(bbox_to_anchor=(.826, .05), loc=2, borderaxespad=-1.)
         plt.grid(True)
-        #plt.show()
 
         plot_title.__add__('.png')
         plt.savefig(plot_title)
 
+# Main function
+def main(epochs):
+    # Initialize Neural Network
+    N = Network(1, 5, 100, 2, 10)
 
-# Initialize Neural Network
-N = Network(1, 5, 100, 2, 10, True)
+    # Generate training data
+    train_feature = np.random.uniform(0, 1, (N.p.rows, N.p.cols))
+    train_target = gen_target(train_feature, N.p.rows, 0.5, 0.6, 0.4)
 
-# Generate training data
-train_feature = np.random.uniform(0, 1, (N.p.rows, N.p.cols))
-train_target = gen_target(train_feature, N.p.rows, 0.5, 0.6, 0.4)
+    # Generate testing data
+    testing_size = 100
+    test_feature = np.random.uniform(0, 1, (testing_size, N.p.cols))
+    test_target = gen_target(test_feature, testing_size, 0.5, 0.6, 0.4)
 
-#print(train_feature)
-#print(train_target)
+    # Print some statistics prior to training
+    print("\n --------------- Statistics prior to training ---------------")
+    N.test(train_feature, train_target, N.p.rows, 
+          'training data prior to training')
+    N.tot_err(train_feature, train_target, N.p.rows)
 
-# Generate testing data
-testing_size = 100
-test_feature = np.random.uniform(0, 1, (testing_size, N.p.cols))
-test_target = gen_target(test_feature, testing_size, 0.5, 0.6, 0.4)
+    N.test(test_feature, test_target, testing_size, 
+          'testing data prior to training')
+    N.tot_err(test_feature, test_target, testing_size)
 
+    # Traing and test over epochs provided as input
+    for i in range(epochs.size):
+        # Reset weights for next training interval
+        N.reset_w()
 
-N.test(train_feature, train_target, N.p.rows, 'training data')
-N.tot_err(train_feature, train_target, N.p.rows)
+        for j in range(epochs[i]):
+            N.train(train_feature, train_target)
 
-N.test(test_feature, test_target, testing_size, 'testing data')
-N.tot_err(test_feature, test_target, testing_size)
-#print(" Wh = \n", N.w.Wh)
+        # Print some statistics after training
+        print("\n -------------- After training over %d epochs --------------" 
+              %epochs[i])
+    
+        N.test(train_feature, train_target, N.p.rows, 
+              'training data after training')
+        N.tot_err(train_feature, train_target, N.p.rows)
 
-for i in range(1000):
-    N.train(train_feature, train_target)
-    #N.tot_err(train_feature, train_target, N.p.rows)
-    #N.tot_err(test_feature, test_target, testing_size)
+        N.test(test_feature, test_target, testing_size, 
+              'testing data after training')
+        N.tot_err(test_feature, test_target, testing_size)
 
-N.test(train_feature, train_target, N.p.rows, 'training data')
-N.tot_err(train_feature, train_target, N.p.rows)
+        N.plot_test(train_feature, train_target, N.p.rows, 
+                    'Plot of Classification of Training Data '
+                    'over %d epochs' %epochs[i])
+        N.plot_test(test_feature, test_target, testing_size, 
+                    'Plot of Classification of Testing Data '
+                    'over %d epochs' %epochs[i])
 
-N.test(test_feature, test_target, testing_size, 'testing data')
-N.tot_err(test_feature, test_target, testing_size)
+    print("\n")
 
-N.plot_test(train_feature, train_target, N.p.rows, 
-            'Plot of Classification of Training Data')
-N.plot_test(test_feature, test_target, testing_size, 
-            'Plot of Classification of Testing Data')
+# Run main function
+main(np.array([100, 250, 500, 2500, 5000]))
 
